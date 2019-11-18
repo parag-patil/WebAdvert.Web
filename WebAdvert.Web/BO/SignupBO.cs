caee60354116eb1cab86b814562467ba0c0c1057
Parag Patil <parag.patil@live.com>
2019-11-17 23:31:25 -0800

    Integrating AWS Cognito with ASP.NET Core. Initial Version.

---
+++ WebAdvert.Web/BO/SignupBO.cs
@@ -0,0 +1,46 @@
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Amazon.AspNetCore.Identity.Cognito;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAdvert.Web.BO
+{
+    public class SignupBO
+    {
+        private readonly string USER_EXISTS = "User with this email already exists.";
+        private readonly CognitoUserPool _pool;
+        private readonly UserManager<CognitoUser> _userManager;
+        public SignupBO(CognitoUserPool pool, UserManager<CognitoUser> userManager)
+        {
+            _pool = pool;
+            _userManager = userManager;
+        }
+        public async Task<SignupViewModel> CreateUser(SignupViewModel model)
+        {
+            var user = _pool.GetUser(model.Email);          //We are using Email as userid.
+
+            if(user.Status != null)
+            {
+                model.IsValid = false;
+                model.Message = USER_EXISTS;
+
+                return model;
+            }
+
+            user.Attributes.Add(CognitoAttribute.Name.ToString(),model.Email);         //We are using Email as name just to get started.
+            var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+
+            if (createdUser.Succeeded)
+            {
+                model.IsValid = true;
+                model.Message = string.Format("{0} is created successfully.", model.Email);
+            }
+
+            return model;
+        }
+    }
+}
---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.BO;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly SignInManager<CognitoUser> _signinManager;
+        private readonly CognitoUserPool _pool;
+        public AccountsController(UserManager<CognitoUser> userManager, SignInManager<CognitoUser> signinManager, CognitoUserPool pool)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _pool = pool;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            return View(new SignupViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupViewModel signupViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                SignupViewModel vModel = new SignupViewModel();
+                SignupBO bo = new SignupBO(_pool, _userManager);
+                vModel = await bo.CreateUser(signupViewModel);
+                if (!vModel.IsValid)
+                {
+                    ModelState.AddModelError("Signup", vModel.Message);
+                    return View(vModel);
+                }
+                else
+                {
+                    RedirectToAction("Confirm");
+                }
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Confirm()
+        {
+            return View(new ConfirmViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmViewModel confirmViewModel)
+        {
+            return View(confirmViewModel);
+        }
+    }
+}
